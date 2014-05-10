569d1cf53bd408ba82b89edbd844653b60ee70f7
Vincent De Maertelaere <vincentdm@gmail.com>
2014-05-10 17:05:14 +0200

    Adding LTDL and stub for mysqlloader_init()

--- src/include/mysqlloader.h
+++ src/include/mysqlloader.h
@@ -1,5 +1,12 @@
 #ifndef libmysqlloader
 #define libmysqlloader
 
+#include <mysql/mysql.h>
+
+int mysqlloader_init();
+
+/**
+ * function pointers
+ */
 
 #endif
--- src/mysqlloader.c
+++ src/mysqlloader.c
@@ -1,1 +1,14 @@
 #include "mysqlloader.h"
+
+#include "ltdl.h"
+
+static int mysqlloader_inited = 0;
+
+
+int mysqlloader_init() {
+	if (mysqlloader_inited != 0)
+		return 0;
+
+
+	return 0;
+}
